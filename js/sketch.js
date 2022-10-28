8b096ff60c08b7347273483add9933631a38612b
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-10-28 10:19:37 +0100

    Add files via upload
    added comments about p5 functions created square and circle with different colours

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,19 @@
+//setup function runs once at start
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
 }
+var num 
+num = 6
 
+//Draw function is a loop that continously runs 60 frames a second when the program is running. 
 function draw() {
-    background(220);
+    background(220); // light grey background
+    num= num+1
+    
+    fill(255, 255, 255)
+    ellipse(num, num-8, 200);// creates the circle 
+    fill(50, num, num); // setting fill to RGB white
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    square(300, 300, 200);
 }
